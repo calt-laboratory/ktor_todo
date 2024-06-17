3430c2d8bc4bdfd676c066aec45b4630255501df
calt-laboratory <christoph_alt@posteo.de>
2024-06-18 00:24:19 +0200

    feat(database manager): added functions getSingleToDo() + addToDo()

--- src/main/kotlin/com/ktor_todo_app/plugins/database/DatabaseManager.kt
+++ src/main/kotlin/com/ktor_todo_app/plugins/database/DatabaseManager.kt
@@ -1,7 +1,9 @@
 package com.ktor_todo_app.plugins.database
 
+import com.ktor_todo_app.plugins.entities.ToDoDraft
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.transactions.transaction
+
 
 /**
  Establishes the connection to the database.
@@ -13,10 +15,11 @@ class DatabaseManager {
     private val databaseName = "ktor_todo"
     private val userName = "postgres"
     private val password = "password"
+    private val port = 5432
 
     // database
     private val exposedDatabase: Database by lazy {
-        val dataBaseURL = "jdbc:postgresql://$hostname:5432/$databaseName"
+        val dataBaseURL = "jdbc:postgresql://$hostname:$port/$databaseName"
         Database.connect(
             url = dataBaseURL,
             driver = "org.postgresql.Driver",
@@ -26,8 +29,24 @@ class DatabaseManager {
     }
 
     fun  getAllTodos() : List<DBToDoEntity>{
-        return transaction(exposedDatabase) {
+        return transaction(db = exposedDatabase) {
             DBToDoEntity.all().toList()
         }
+    }
+
+    fun getSingleToDo(id: Int): DBToDoEntity? {
+        return transaction(db = exposedDatabase) {
+            DBToDoEntity.findById(id = id)
+        }
+    }
+
+    fun addToDo(toDoDraft: ToDoDraft) : DBToDoEntity {
+        val newToDo = transaction(db = exposedDatabase) {
+            DBToDoEntity.new {
+                title = toDoDraft.title
+                done = toDoDraft.done
+            }
+        }
+        return newToDo
     }
 }
